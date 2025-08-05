e41acb72c9b446ed54c21fdd1e24745bcf28d3bb
puneetkumar10 <puneet25june@gmail.com>
2025-08-05 19:19:28 +0530

    fruits problem

---
+++ NamasteDSA/Array/12_place_fruits.js
@@ -0,0 +1,35 @@
+/**
+ * 3477. Fruits Into Baskets II
+ * You are given two arrays of integers, fruits and baskets, each of length n, where fruits[i] represents the quantity of the ith type of fruit, and baskets[j] represents the capacity of the jth basket.
+
+From left to right, place the fruits according to these rules:
+
+Each fruit type must be placed in the leftmost available basket with a capacity greater than or equal to the quantity of that fruit type.
+Each basket can hold only one type of fruit.
+If a fruit type cannot be placed in any basket, it remains unplaced.
+Return the number of fruit types that remain unplaced after all possible allocations are made.
+
+ * @param {number[]} fruits
+ * @param {number[]} baskets
+ * @return {number}
+ */
+var numOfUnplacedFruits = function(fruits, baskets) {
+    let used = new Array(baskets.length).fill(false);
+    let unplacedCount = 0;
+
+    for (let i = 0; i < fruits.length; i++) {
+        let placed = false;
+        for (let j = 0; j < baskets.length; j++) {
+            if (!used[j] && baskets[j] >= fruits[i]) {
+                used[j] = true;
+                placed = true;
+                break;
+            }
+        }
+        if (!placed) {
+            unplacedCount++;
+        }
+    }
+
+    return unplacedCount;
+};
