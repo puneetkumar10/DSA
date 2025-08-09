eed44d9b13a38db6ec20ec55e1a22a2c58d24de5
puneetkumar10 <puneet25june@gmail.com>
2025-08-09 17:36:14 +0530

    is Power daily leetcode problem

---
+++ NamasteDSA/Maths/isPower.js
@@ -0,0 +1,19 @@
+var isPowerOfThree = function(n) {
+    if (n < 1) return false
+
+    while (n % 3 == 0) {
+        n = n / 3;
+    }
+
+    return n === 1;
+};
+
+var isPowerOfTwo = function(n) {
+    if (n < 1) return false;
+
+    while (n % 2 == 0) {
+        n = n / 2;
+    }
+
+    return n === 1;
+};
