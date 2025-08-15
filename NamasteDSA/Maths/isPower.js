ee85c1c9d15765608ec593e0187b784fe8e3e583
puneetkumar10 <puneet25june@gmail.com>
2025-08-15 11:41:17 +0530

    is power added

--- NamasteDSA/Maths/isPower.js
+++ NamasteDSA/Maths/isPower.js
@@ -17,3 +17,13 @@ var isPowerOfTwo = function(n) {
 
     return n === 1;
 };
+
+var isPowerOfFour = function(n) {
+    if (n < 1) return false
+
+    while (n % 4 == 0) {
+        n = n / 4;
+    }
+
+    return n === 1;
+};
