f237c4f8122774476ed3ba0a8ac3bde4b5f6ac64
puneetkumar10 <puneet25june@gmail.com>
2025-08-22 01:58:32 +0530

    logic explained (bijection check)

--- NamasteDSA/Array/17_word_match.js
+++ NamasteDSA/Array/17_word_match.js
@@ -3,6 +3,14 @@
  * @param {string} s
  * @return {boolean}
  */
+
+/** 
+ * Create 2 objects 
+ * Check for length mismatch for pattern and s
+ * Iterate 1 by 1 for both pattern and s , save them in respective objects 
+ * Check if any of those 2 objects have existing entry for incoming values , also check incoming values must be same or else return false
+ * If not then initialize the values in both maps
+*/
 var wordPattern = function(pattern, s) {
     const words = s.split(" ");
     if (pattern.length !== words.length) return false;
