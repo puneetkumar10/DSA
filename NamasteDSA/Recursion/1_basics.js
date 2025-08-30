b8a9d8557b96784981fb0c2ba0617c72b41fea66
puneetkumar10 <puneet25june@gmail.com>
2025-08-30 20:27:05 +0530

    recursion

---
+++ NamasteDSA/Recursion/1_basics.js
@@ -0,0 +1,6 @@
+// Function calls itself , to solve smaller versions of the same problems 
+/** 
+ * Base case - Wheen function should be stopped
+ * Recursive Case - part where function calls itself
+*/
+
