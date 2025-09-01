49cc9fb9526ce4964962d3ea77d37e8e27a47acf
puneetkumar10 <puneet25june@gmail.com>
2025-09-01 21:01:53 +0530

    heap

--- NamasteDSA/Array/19_maximum_avg_pass_ratio.js
+++ NamasteDSA/Array/19_maximum_avg_pass_ratio.js
@@ -34,3 +34,45 @@ var maxAverageRatio = function(classes, extraStudents) {
     }
     return sum / classes.length;
 };
+
+
+// HEAP SOLUTION CHATGPT 
+
+/**
+ * @param {number[][]} classes
+ * @param {number} extraStudents
+ * @return {number}
+ */
+var maxAverageRatio = function(classes, extraStudents) {
+  const gain = (p, t) => (p + 1) / (t + 1) - p / t;
+
+  // tiny max-heap for triples [gain, p, t]
+  const h = [];
+  const up = i => { while (i) { let p = (i - 1) >> 1;
+    if (h[p][0] >= h[i][0]) break; [h[p], h[i]] = [h[i], h[p]]; i = p; } };
+  const down = i => { for (;;) { let l = i * 2 + 1, r = l + 1, b = i;
+    if (l < h.length && h[l][0] > h[b][0]) b = l;
+    if (r < h.length && h[r][0] > h[b][0]) b = r;
+    if (b === i) break; [h[i], h[b]] = [h[b], h[i]]; i = b; } };
+  const push = (g, p, t) => { h.push([g, p, t]); up(h.length - 1); };
+  const pop = () => { const top = h[0]; const last = h.pop();
+    if (h.length) { h[0] = last; down(0); } return top; };
+
+  // build heap
+  for (const [p, t] of classes) push(gain(p, t), p, t);
+
+  // assign students greedily
+  while (extraStudents-- > 0) {
+    let [, p, t] = pop();
+    p++; t++;
+    push(gain(p, t), p, t);
+  }
+
+  // compute average
+  let sum = 0;
+  while (h.length) {
+    const [, p, t] = pop();
+    sum += p / t;
+  }
+  return sum / classes.length;
+};
