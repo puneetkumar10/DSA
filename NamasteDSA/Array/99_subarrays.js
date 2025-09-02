ba8f719d0e409a2bdfaab9e0210b321e047e9148
puneetkumar10 <puneet25june@gmail.com>
2025-09-02 19:29:52 +0530

    max subarray sum

--- NamasteDSA/Array/99_subarrays.js
+++ NamasteDSA/Array/99_subarrays.js
@@ -6,3 +6,19 @@ for (let start = 0; start < arr.length; start++) {
     console.log(subarray);
   }
 }
+
+
+// function maxSubArray(nums) {
+//   let current = nums[0];
+//   let best = nums[0];
+
+//   for (let i = 1; i < nums.length; i++) {
+//     current = Math.max(nums[i], current + nums[i]);
+//     best = Math.max(best, current);
+//   }
+//   return best;
+// }
+
+// maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
+
+// module.exports = maxSubArray;
