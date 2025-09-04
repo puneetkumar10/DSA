6077b65c8fdf2308c7be95a6ab920b314684c571
puneetkumar10 <puneet25june@gmail.com>
2025-09-04 19:11:25 +0530

    leetcode daily

---
+++ NamasteDSA/Maths/3516_find_closest.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} z
+ * @return {number}
+ */
+var findClosest = function(x, y, z) {
+    let person1_diff = Math.abs(z - x);
+    let person2_diff = Math.abs(z - y);
+    return (person1_diff == person2_diff) ? 0 : (person1_diff > person2_diff) ? 2 : 1;
+};
