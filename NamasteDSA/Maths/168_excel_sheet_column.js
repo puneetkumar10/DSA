08b2f3217b3107a7091e23b2846b8d3084a46771
puneetkumar10 <puneet25june@gmail.com>
2025-09-06 16:42:46 +0530

    Excel sheet problem 1

---
+++ NamasteDSA/Maths/168_excel_sheet_column.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function(columnNumber) {
+  let result = "";
+
+  while (columnNumber > 0) {
+    columnNumber--; 
+    let remainder = columnNumber % 26;
+    result = String.fromCharCode(65 + remainder) + result; 
+    columnNumber = Math.floor(columnNumber / 26);
+  }
+
+  return result;
+};
