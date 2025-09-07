66fd66a5d665dfafbf8a47bee9820dfa33e80137
puneetkumar10 <puneet25june@gmail.com>
2025-09-07 22:20:24 +0530

    leetcode daily

---
+++ NamasteDSA/Maths/1304_arr_sum_zero.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var sumZero = function(n) {
+    if (n == 1) {
+        return [0]
+    }
+    let resp = []
+    if (n % 2 == 0) {
+        // even
+        for (let i = 1 ; i <= Math.floor(n/2) ; i++) {
+            resp.push(i);
+            resp.push(i * -1)
+        }
+    } else {
+        // odd 
+        for (let i = 1 ; i <= Math.floor(n/2) ; i++) {
+            resp.push(i);
+            resp.push(i * -1)
+        }
+        resp.push(0)
+    }
+
+    return resp;
+};
