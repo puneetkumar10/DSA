049f0d23d9d2ef07bc3ac7ac9c64a1d42c995833
puneetkumar10 <puneet25june@gmail.com>
2025-09-11 21:10:57 +0530

    819 leetcode most common

---
+++ NamasteDSA/Array/21_mostCommonWord.js
@@ -0,0 +1,32 @@
+/** Leetcode 819
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function(paragraph, banned) {
+    let max = 0;
+    let maxWord = "";
+
+    // remove "." , "," using regex
+    const words = paragraph.toLowerCase().match(/\w+/g);
+    let wordMap = {}
+
+    // populate wordMap object with words and their count
+    for (let i = 0 ; i < words.length ; i ++) {
+        wordMap[words[i]] = (wordMap[words[i]] || 0) + 1;
+    }
+
+    // remove banned words from map 
+    for (let j = 0 ; j < banned.length ; j ++) {
+        delete wordMap[banned[j]]
+    }
+
+    // now we find the max and return 
+    for (let ele in wordMap) {
+        if (wordMap[ele] > max) {
+            max = wordMap[ele]
+            maxWord = ele
+        }
+    }
+    return maxWord;
+};
