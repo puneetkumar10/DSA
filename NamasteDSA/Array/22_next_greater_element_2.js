7b9c0174960f6b573bf11179ef415aefd7dfe317
puneetkumar10 <puneet25june@gmail.com>
2025-09-14 10:14:38 +0530

    next grater element 2

--- NamasteDSA/Array/22_next_greater_element_2.js
+++ NamasteDSA/Array/22_next_greater_element_2.js
@@ -1,1 +1,31 @@
 // Leetcode 503
+
+var nextGreaterElements = function(nums) {
+  let resp = [];
+  for (let i = 0; i < nums.length; i++) {
+    let found = false;
+
+    // pos -> end
+    for (let start = i + 1; start < nums.length; start++) {
+      if (nums[start] > nums[i]) {
+        resp.push(nums[start]);
+        found = true;
+        break;
+      }
+    }
+
+    // start -> pos (only if not found yet)
+    if (!found) {
+      for (let start2 = 0; start2 < i; start2++) {
+        if (nums[start2] > nums[i]) {
+          resp.push(nums[start2]);
+          found = true;
+          break;
+        }
+      }
+    }
+
+    if (!found) resp.push(-1);
+  }
+  return resp;
+};
