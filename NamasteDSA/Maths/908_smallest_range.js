c1eb23eb42a6fc052a9c7c1531ee8c42653bffaf
puneetkumar10 <puneet25june@gmail.com>
2025-09-21 18:47:20 +0530

    daily question

---
+++ NamasteDSA/Maths/908_smallest_range.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+let smallestRangeI = function (nums, k) {
+    let min = Math.min(...nums);
+    let max = Math.max(...nums);
+    let diff = max - min - 2 * k;
+
+    return diff > 0 ? diff : 0;
+}
