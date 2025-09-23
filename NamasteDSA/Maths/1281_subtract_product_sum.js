4d2f7a5689682f81b43c59e10e25a74c65b818a7
puneetkumar10 <puneet25june@gmail.com>
2025-09-23 12:01:30 +0530

    leetcode daily

---
+++ NamasteDSA/Maths/1281_subtract_product_sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var subtractProductAndSum = function(n) {
+    let product = 1;
+    let sum = 0;
+    while (n > 0) {
+        let digit = n % 10;
+        n = Math.trunc(n / 10) ;
+        product = product * digit;
+        sum = sum + digit;
+    }
+    return product - sum;
+};
