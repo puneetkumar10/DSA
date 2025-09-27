f33e1ead5697abf7f40368672d0d3f51aae74c15
puneetkumar10 <puneet25june@gmail.com>
2025-09-27 09:29:17 +0530

    daily

---
+++ NamasteDSA/Maths/2160_min_sum_of_two.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var minimumSum = function(num) {
+    let arr = num.toString().split('').map((e)=>Number(e));
+    arr.sort((a,b)=>a-b);
+    return ((10*arr[0])+(10*arr[1])+arr[2]+arr[3]);
+};
