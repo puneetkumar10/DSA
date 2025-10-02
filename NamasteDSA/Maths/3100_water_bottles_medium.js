ceb1f3c0bd6fe087a11a8a54125337f205309215
puneetkumar10 <puneet25june@gmail.com>
2025-10-02 20:19:36 +0530

    leetcode daily

---
+++ NamasteDSA/Maths/3100_water_bottles_medium.js
@@ -0,0 +1,12 @@
+var maxBottlesDrunk = function(numBottles, numExchange) {
+    let drunk = numBottles;
+    let empty = numBottles;
+
+    while (empty >= numExchange) {
+        drunk += 1;
+        empty -= numExchange - 1; 
+        numExchange += 1;
+    }
+
+    return drunk;
+};
