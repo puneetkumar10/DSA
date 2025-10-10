9f1d0c0488286963a10bdd981528c88da2e8fd3e
puneetkumar10 <puneet25june@gmail.com>
2025-10-10 18:46:07 +0530

    daily

---
+++ NamasteDSA/Array/961_repeated_element.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var repeatedNTimes = function(nums) {
+    let eleMap = {}
+
+    for (let i = 0 ; i < nums.length ; i++) {
+        if (eleMap[nums[i]]) {
+            return nums[i]
+        } else {
+            eleMap[nums[i]] = 1
+        }
+    }
+};
