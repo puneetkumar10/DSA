241d6a5b38d0ca5ef61af29fed5aa42f4e1f396f
puneetkumar10 <puneet25june@gmail.com>
2025-10-20 14:35:12 +0530

    daily leetcode

---
+++ NamasteDSA/Array/2011_operations.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function(operations) {
+    let output = 0;
+    for (let i = 0 ; i < operations.length ; i ++) {
+        if (operations[i] == "++X" || operations[i] == "X++") {
+            output += 1;
+        } else if (operations[i] == "--X" || operations[i] == "X--") {
+            output -= 1;
+        } else {
+            return 0; // invalid operation
+        }
+    }
+
+    return output;
+};
