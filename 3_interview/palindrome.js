7ab67b2381021f92ef35672bc02c89407566e353
puneetkumar10 <puneet25june@gmail.com>
2025-10-22 20:05:19 +0530

    palindrome

---
+++ 3_interview/palindrome.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false
+    }
+    let numStr = x.toString();
+    let strRev = numStr.split("").reverse().join("");
+    if (numStr === strRev) {
+        return true
+    } else {
+        return false
+    }
+};
