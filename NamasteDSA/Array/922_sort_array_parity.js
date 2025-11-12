6c296a08f45706a44f3c7719fc612a75070c02c6
puneetkumar10 <puneet25june@gmail.com>
2025-11-12 22:44:02 +0530

    daily

---
+++ NamasteDSA/Array/922_sort_array_parity.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParityII = function(nums) {
+    let evenArr = []
+    let oddArr = []
+    for (let i = 0 ; i < nums.length ; i ++) {
+        nums[i] % 2 == 0 ? evenArr.push(nums[i]) : oddArr.push(nums[i]);
+    }
+    let evenIndex = 0
+    let oddIndex = 0
+    for (let j = 0 ; j < nums.length ; j++) {
+        if (j % 2 == 0) {
+            nums[j] = evenArr[evenIndex]
+            evenIndex++
+        } else {
+            nums[j] = oddArr[oddIndex]
+            oddIndex++
+        }
+    }
+    return nums
+};
