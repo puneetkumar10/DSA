4063b083210c8668a4cce306206e460fbc0113c6
puneetkumar10 <puneet25june@gmail.com>
2025-11-18 22:35:15 +0530

    daily

--- NamasteDSA/Array/3289_sneaky_numbers.js
+++ NamasteDSA/Array/3289_sneaky_numbers.js
@@ -2,16 +2,30 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// var getSneakyNumbers = function(nums) {
+//     let eleMap = {}
+//     let resp = []
+//     for (let i = 0 ; i < nums.length ; i ++) {
+//         eleMap[nums[i]] = (eleMap[nums[i]] || 0) + 1;
+//     }
+//     for (let ele in eleMap) {
+//         if (eleMap[ele] >= 2) {
+//             resp.push(parseInt(ele))
+//         }
+//     }
+//     return resp
+// };
+
 var getSneakyNumbers = function(nums) {
-    let eleMap = {}
-    let resp = []
-    for (let i = 0 ; i < nums.length ; i ++) {
-        eleMap[nums[i]] = (eleMap[nums[i]] || 0) + 1;
+  const freq = {};
+  const resp = [];
+
+  for (const num of nums) {
+    freq[num] = (freq[num] || 0) + 1;
+    if (freq[num] === 2) {  
+      resp.push(num);
     }
-    for (let ele in eleMap) {
-        if (eleMap[ele] >= 2) {
-            resp.push(parseInt(ele))
-        }
-    }
-    return resp
+  }
+
+  return resp;
 };
