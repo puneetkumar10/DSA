4dfd4190e6783068dcd15d537ea021d39f770965
puneetkumar10 <puneet25june@gmail.com>
2025-11-19 23:01:06 +0530

    approach cleaned

--- NamasteDSA/Array/2154_double_if_found.js
+++ NamasteDSA/Array/2154_double_if_found.js
@@ -3,18 +3,26 @@
  * @param {number} original
  * @return {number}
  */
+// var findFinalValue = function(nums, original) {
+//     let eleMap = {}
+//     let status = true
+//     for (let i = 0 ; i < nums.length ; i ++) {
+//         eleMap[nums[i]] = (eleMap[nums[i]] || 0) + 1
+//     }
+//     while (status) {
+//         if (eleMap.hasOwnProperty(original)) {
+//             original = original * 2
+//         } else {
+//             status = false
+//         }
+//     }
+//     return original
+// };
+
 var findFinalValue = function(nums, original) {
-    let eleMap = {}
-    let status = true
-    for (let i = 0 ; i < nums.length ; i ++) {
-        eleMap[nums[i]] = (eleMap[nums[i]] || 0) + 1
-    }
-    while (status) {
-        if (eleMap.hasOwnProperty(original)) {
-            original = original * 2
-        } else {
-            status = false
-        }
-    }
-    return original
+  const set = new Set(nums);
+  while (set.has(original)) {
+    original *= 2;
+  }
+  return original;
 };
