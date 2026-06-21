0cb600bc6d65e35384aa205eba431e4e31117067
puneetkumar10 <puneet25june@gmail.com>
2026-06-21 18:11:35 +0530

    selection sort fixed

--- NamasteDSA/sorting/selection.js
+++ NamasteDSA/sorting/selection.js
@@ -1,7 +1,7 @@
 let nums = [7, 5, 2, 9, 10];
 let temp = 0;
 for (let i = 0; i < nums.length - 1; i++) {
-  for (let j = i; j < nums.length; j++) {
+  for (let j = i+1; j < nums.length; j++) {
     if (nums[i] > nums[j]) {
       // swap
       temp = nums[i];
